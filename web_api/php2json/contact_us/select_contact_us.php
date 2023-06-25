f5acde233c8c63228222af32678688fcee53da36
VahidsVs <svs.3001@gmail.com>
2023-06-25 22:41:48 +0330

    api v1.23.06.25

--- web_api/php2json/contact_us/select_contact_us.php
+++ web_api/php2json/contact_us/select_contact_us.php
@@ -7,25 +7,21 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$headers=getallheaders();
+$headers = getallheaders();
 session_start();
-$accessAuthorization=new Authorization();
-$isAuthorized["auth"]=$isAuthorized["aa"]=false;
+$accessAuthorization = new Authorization();
+$isAuthorized["auth"] = $isAuthorized["aa"] = false;
 if (array_key_exists("Authorization", $headers) && array_key_exists("token", $_SESSION) && array_key_exists("key", $_SESSION))
     $isAuthorized = $accessAuthorization->isAuthorized($headers["Authorization"], $_SESSION["token"], $_SESSION["key"], RolesTitle::role_contactUs);
 
-if($isAuthorized["auth"]&&$isAuthorized["aa"])
-{
-$accessClass = new ContactUs("select", $_GET);
-http_response_code($accessClass->getHttpResponseCode());
-echo json_encode($accessClass->getJsonData());
-}
-
-else
-{
-http_response_code(401);
-$unathorizedMsg=["message"=>"Unauthorized access!"];
-echo json_encode($unathorizedMsg);
+if ($isAuthorized["auth"] && $isAuthorized["aa"]) {
+    $accessClass = new ContactUs("select", $_GET);
+    http_response_code($accessClass->getHttpResponseCode());
+    echo json_encode($accessClass->getJsonData());
+} else {
+    http_response_code(401);
+    $unathorizedMsg = ["message" => "Unauthorized access!"];
+    echo json_encode($unathorizedMsg);
 }
 
 ?>
