0f23cb902efe5968376685fa32974434edd47ac9
VahidsVs <svs.3001@gmail.com>
2023-07-14 16:31:13 +0330

    api v1.23.07.09

--- web_api/php2json/media_management/insert_media.php
+++ web_api/php2json/media_management/insert_media.php
@@ -20,45 +20,50 @@ if (array_key_exists("Authorization", $headers) && array_key_exists("token", $_S
 if ($isAuthorized["auth"] && $isAuthorized["aa"]) {
   $targetDir = "";
   $uploadPath = "../../../uploads";
-$targetFile = $targetDir . basename($_FILES["fileUpload"]["name"]);
-  $isUploadOk = true;
-  $fileExtension = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-  $fileName = $_FILES["fileUpload"]["name"];
-  $fileSize = $_FILES["fileUpload"]["size"];
-  $fileTempName = $_FILES["fileUpload"]["tmp_name"];
-  $renamedFileName = str_replace("." . $fileExtension, "", $fileName) . uniqid() . "." . $fileExtension;
+  if ($_FILES) {
+    $targetFile = $targetDir . basename($_FILES["fileUpload"]["name"]);
+    $isUploadOk = true;
+    $fileExtension = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $fileName = $_FILES["fileUpload"]["name"];
+    $fileSize = $_FILES["fileUpload"]["size"];
+    $fileTempName = $_FILES["fileUpload"]["tmp_name"];
+    $renamedFileName = str_replace("." . $fileExtension, "", $fileName) . uniqid() . "." . $fileExtension;
 
+    // Check file size
+    if ($fileSize > $iniConfig["max-file-size"]) {
+      $jsonData["get"]["fileUploadSize"] = Codes::msg_invalidFileSize;
+      http_response_code(400);
+      $isUploadOk = false;
+    }
 
-  // Check file size
-  if ($fileSize > $iniConfig["max-file-size"]) {
-    $jsonData["get"]["fileUploadSize"] = Codes::msg_invalidFileSize;
-    http_response_code(400);
-    $isUploadOk = false;
-  }
+    // Allow certain file formats
+    if (!in_array($fileExtension, ["jpg", "jpeg", "png"])) {
+      $jsonData["errors"]["fileUploadExtension"] = Codes::msg_invalidFileExtension;
+      http_response_code(400);
+      $isUploadOk = false;
+    }
 
-  // Allow certain file formats
-  if (!in_array($fileExtension, ["jpg", "jpeg", "png"])) {
-    $jsonData["errors"]["fileUploadExtension"] = Codes::msg_invalidFileExtension;
-    http_response_code(400);
-    $isUploadOk = false;
-  }
-
-  // Check if $isUploadOk is set to 0 by an error
-  if ($isUploadOk) {
-    // if everything is ok, try to upload file
-    if (move_uploaded_file($fileTempName, "$uploadPath/$renamedFileName")) {
-      $params["fileName"]=$renamedFileName;
-      $params["fileExtension"]=$fileExtension;
-      $params["filePath"]="/uploads";
-      $accessClass = new Media("insert", $params);
-      http_response_code($accessClass->getHttpResponseCode());
-      echo json_encode($accessClass->getJsonData());
+    // Check if $isUploadOk is set to 0 by an error
+    if ($isUploadOk) {
+      // if everything is ok, try to upload file
+      if (move_uploaded_file($fileTempName, "$uploadPath/$renamedFileName")) {
+        $params["fileName"] = $renamedFileName;
+        $params["fileExtension"] = $fileExtension;
+        $params["filePath"] = "/uploads";
+        $accessClass = new Media("insert", $params);
+        http_response_code($accessClass->getHttpResponseCode());
+        echo json_encode($accessClass->getJsonData());
+      } else {
+        $jsonData["errors"]["file"] = Codes::msg_ErrorUploadingFile;
+        http_response_code(400);
+        echo json_encode($jsonData);
+      }
     } else {
-      $jsonData["errors"]["media"] = Codes::msg_ErrorUploadingFile;
       http_response_code(400);
       echo json_encode($jsonData);
     }
   } else {
+    $jsonData["errors"]["file"] = Codes::msg_isRequired;
     http_response_code(400);
     echo json_encode($jsonData);
   }
