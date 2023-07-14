b718eddee42db7628a5e04b959566eafa61f0875
VahidsVs <svs.3001@gmail.com>
2023-07-14 17:46:35 +0330

    api v1.23.07.09

---
+++ web_api/php2json/config/get_config.php
@@ -0,0 +1,30 @@
+<?php
+include_once("../../interface/class_contact_us.php");
+include_once("../../class_authorization.php");
+include_once("../../class_roles_title.php");
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+$iniConfig = parse_ini_file("../../config.ini");
+
+$headers = getallheaders();
+session_start();
+$accessAuthorization = new Authorization();
+$isAuthorized["auth"] = $isAuthorized["aa"] = false;
+if (array_key_exists("Authorization", $headers) && array_key_exists("token", $_SESSION) && array_key_exists("key", $_SESSION))
+    $isAuthorized = $accessAuthorization->isAuthorized($headers["Authorization"], $_SESSION["token"], $_SESSION["key"], null);
+
+if ($isAuthorized["auth"] && $isAuthorized["aa"]) {
+    $requestedConfig = explode(",", $_GET["getConfig"]);
+    foreach ($requestedConfig as $config)
+        $jsonData[$config] = $iniConfig[$config];
+        
+    echo json_encode($jsonData);
+} else {
+    http_response_code(401);
+    $unathorizedMsg = ["message" => "Unauthorized access!"];
+    echo json_encode($unathorizedMsg);
+}
+
+?>
