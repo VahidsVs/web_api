f1e4befd637dcc596e857d9026d61d3610952c17
VahidsVs <svs.3001@gmail.com>
2023-07-17 01:54:18 +0330

    api v1.23.07.06

--- web_api/php2json/post/select_post.php
+++ web_api/php2json/post/select_post.php
@@ -7,7 +7,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-    $_GET["limit"]>=1?$limit="Limit {$_GET["limit"]}":$limit=null;
+    
+    array_key_exists("limit",$_GET)?$limit="Limit {$_GET["limit"]}":$limit=null;
     $accessClass = new Post("select", $_GET,"Order By updated_at Desc",$limit);
     http_response_code($accessClass->getHttpResponseCode());
     echo json_encode($accessClass->getJsonData());
