364ad744b7216f1e1793610768a07a2b67011141
VahidsVs <svs.3001@gmail.com>
2023-07-28 17:53:16 +0330

    api v1.23.07.06

---
+++ web_api/php2json/post/select_post_pagination.php
@@ -0,0 +1,15 @@
+<?php
+include_once("../../interface/class_post.php");
+include_once("../../class_authorization.php");
+include_once("../../class_roles_title.php");
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    
+    $accessClass = new Post("select", $_GET);
+    http_response_code($accessClass->getHttpResponseCode());
+    echo json_encode($accessClass->getJsonData());
+
+?>
